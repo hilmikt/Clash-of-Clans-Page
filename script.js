5d618f3702bd1b54d64c77de3c0f6a5dca604f3a
Hilmi <muhammedhilmi46@gmail.com>
2023-07-24 09:39:07 -0700

    Added some transition effects

---
+++ script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.coc-text', {delay:200, origin: 'top'})
+sr.reveal('.coc-img', {delay:450, origin: 'top'})
+sr.reveal('.icons', {delay:500, origin: 'left'})
+sr.reveal('.scroll-down', {delay:500, origin: 'right'})
